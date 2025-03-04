a57036e72b99e7dae220f1a42677d976c5b6864a
NirmalTechie <nirmalofficial22@gmail.com>
2025-03-04 23:04:11 +0530

    Addition of assets

--- assets.js
+++ assets.js
@@ -27,6 +27,36 @@ class Assets {
         link.onerror = (err) => reject(err);
         document.head.appendChild(link);
       });
+      static loadStyle(href) {
+        return new Promise((resolve, reject) => {
+          const link = document.createElement("link");
+          link.rel = "stylesheet";
+          link.href = href;
+          link.onload = () => resolve(link);
+          link.onerror = (err) => reject(err);
+          document.head.appendChild(link);
+        });
+      }
+      static loadStyle(href) {
+        return new Promise((resolve, reject) => {
+          const link = document.createElement("link");
+          link.rel = "stylesheet";
+          link.href = href;
+          link.onload = () => resolve(link);
+          link.onerror = (err) => reject(err);
+          document.head.appendChild(link);
+        });
+      }
+      static loadStyle(href) {
+        return new Promise((resolve, reject) => {
+          const link = document.createElement("link");
+          link.rel = "stylesheet";
+          link.href = href;
+          link.onload = () => resolve(link);
+          link.onerror = (err) => reject(err);
+          document.head.appendChild(link);
+        });
+      }
     }
   }
   
